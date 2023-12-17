7185691c006f6d64ae991c68688ba37b07d4a6d9
Ana Luísa <86081728+almt00@users.noreply.github.com>
2023-12-17 14:04:15 +0000

    cenas graficos

--- graphs/our_app.py
+++ graphs/our_app.py
@@ -10,102 +10,92 @@ from streamlit.components.v1 import components as components
 import folium
 import numpy as np
 
+data_scopus = pd.read_csv('../first-sprint/icd_marta_ana_scopus_edited.csv');
+data_unesco = pd.read_csv('../first-sprint/unesco_heritage_by_country.csv');
+data_unesco_sorted = data_unesco.sort_values(by="Properties inscribed", ascending=False)
+top_10_data_unesco = data_unesco_sorted.head(10)
+chart_data = pd.DataFrame(data_scopus, columns=['Country'])
+filtered_sum = chart_data["Country"].value_counts().reset_index()
+filtered_sum.columns = ['Country', 'Document Count']
+top_10_data = filtered_sum.head(10)
+merged_data = pd.merge(data_unesco_sorted, filtered_sum, on="Country", suffixes=('_file1', '_file2'))
+top_10_merged_data = merged_data.head(10)
 
 
 st.set_page_config(layout='wide', initial_sidebar_state='expanded')
 
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-    
 
+st.title("Recommender Systems in Cultural Heritage Sites")
 
-st.title("Ana e Marta Dashboard")
+c1, c2 = st.columns((7, 3))
 
-############################
-
-c1, c2, c3, c4 = st.columns((1, 5, 3, 1))
 with c1:
-     st.write("") 
+        countries = top_10_merged_data['Country']
+        info1 = top_10_merged_data['Properties inscribed']  # Data for the first type of information
+        info2 =  top_10_merged_data['Document Count']  # Data for the second type of information
 
-# Data for countries and their information
+        # Set width of bar
+        bar_width = 0.32
+
+        # Set position of bars on X axis
+        r = np.arange(len(countries))
+
+        # Create the bar plot
+        fig, ax = plt.subplots(figsize=(10, 5))  # Set figure size
+        pastel_colors = plt.cm.get_cmap('Pastel1', len(countries))
+
+        ax.bar(r - bar_width/2, info1, color=pastel_colors(0), width=bar_width, edgecolor='grey', label="UNESCO's inscribed properties")
+        ax.bar(r + bar_width/2, info2, color=pastel_colors(1), width=bar_width, edgecolor='grey', label='Scientific Papers Count')
+
+        # Display the values on the bars
+        for i, (v1, v2) in enumerate(zip(info1, info2)):
+            ax.text(r[i] - bar_width/2, v1 + 1, str(v1), ha='center', va='bottom', color='black')
+            ax.text(r[i] + bar_width/2, v2 + 1, str(v2), ha='center', va='bottom', color='black')
+
+        # Customize appearance
+        # ax.set_xlabel('Countries', fontweight='bold')
+        # ax.set_ylabel('Count', fontweight='bold')
+        ax.set_xticks(r)
+        ax.set_xticklabels(countries)
+        ax.set_title("Dsitribution of scientific papers and UNESCO's inscribed properties by country")
+        ax.legend()
+
+        # Hide spines
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+
+        # Display the chart using Streamlit
+        st.pyplot(fig)
+
+       
 with c2:
+     # Selector for choosing the country
+    selected_country = st.selectbox('Select a country:', top_10_merged_data['Country'].unique())
     
-    countries = ['Italy', 'Spain', 'France', 'Germany', 'China', 'India', 'Mexico']
-    info1 = [20, 30, 25, 35, 28, 22, 38]  # Data for the first type of information
-    info2 = [40, 35, 30, 45, 32, 27, 42]  # Data for the second type of information
+    # Filter data based on selected country
+    selected_data = top_10_merged_data[top_10_merged_data['Country'] == selected_country]
 
-    # Set width of bar
-    bar_width = 0.35
+    # Calculate the ratio (Documents per Site)
+    ratio = round(selected_data['Document Count'] / selected_data['Properties inscribed'],2)
 
-    # Set position of bars on X axis
-    r = np.arange(len(countries))
-
-    # Create the bar plot
-    fig, ax = plt.subplots(figsize=(10, 6))  # Set figure size
-    pastel_colors = plt.cm.get_cmap('Pastel1', len(countries))
-
-    ax.bar(r - bar_width/2, info1, color=pastel_colors(0), width=bar_width, edgecolor='grey', label='Info 1')
-    ax.bar(r + bar_width/2, info2, color=pastel_colors(1), width=bar_width, edgecolor='grey', label='Info 2')
-
-    # Display the values on the bars
-    for i, (v1, v2) in enumerate(zip(info1, info2)):
-        ax.text(r[i] - bar_width/2, v1 + 1, str(v1), ha='center', va='bottom', color='black')
-        ax.text(r[i] + bar_width/2, v2 + 1, str(v2), ha='center', va='bottom', color='black')
-
-    # Customize appearance
-    ax.set_xlabel('Countries', fontweight='bold')
-    ax.set_ylabel('Values', fontweight='bold')
-    ax.set_xticks(r)
-    ax.set_xticklabels(countries)
-    ax.set_title('Information Comparison Between Countries')
-    ax.legend()
-
-    # Hide spines
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
-
-    # Display the chart using Streamlit
-    st.pyplot(fig)
+    # Display the ratio
+    # st.markdown(f"Documents per inscribed UNESCO property ratio ({selected_country}): **{ratio.values[0]}**")
+    st.markdown(f"##### **{ratio.values[0]}** scientific papersz     per inscribed UNESCO property")
 
     #############################
 
-with c3:
-    # Sample data for horizontal bar chart
-    bar_data = pd.DataFrame({
-        'Category': ['Andorra', 'Belgica', 'Canada', 'Dinamarca', 'Equador', 'França', 'Portugal'],
-        'Values': [23, 45, 56, 78, 12, 34, 65]
-    })
 
-    # Create a horizontal bar chart with thin bars and reduced spacing
-    fig, ax = plt.subplots(figsize=(8, 4))  # Set figure size to make it smaller
-    pastel_colors = plt.cm.get_cmap('Pastel1', len(bar_data))
-    bars = ax.barh(bar_data['Category'], bar_data['Values'], color=pastel_colors.colors, height=0.2)  # Adjust bar height for thin bars
-
-    # Display the values on top of each bar
-    for i, val in enumerate(bar_data['Values']):
-        ax.text(val, i, str(val), ha='left', va='center', fontsize=10)  # Display values on bars
-
-    # Customize appearance: Remove background gridlines and spines
-    ax.grid(False)
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
-    ax.spines['left'].set_visible(False)  # Hide the y-axis line
-
-    # Hide x-axis line and labels
-    ax.xaxis.set_visible(False)
-
-    # Set y-axis label and title
-    ax.set_title('Horizontal Bar Chart', fontsize=14)
-
-    # Display the bar chart using Streamlit
-    st.pyplot(fig)
-with c4:
-     st.write("") 
-
-
-##################################################
-# Function to generate word cloud
-
+c1, c2 = st.columns((7, 3))
+with c1:
+    fig3 = px.treemap(filtered_sum, path = ["Country"], values = "Document Count",hover_data = ["Document Count"],color = "Country")
+    fig3.update_layout(width = 800, height = 650)
+    fig3.update_traces(textinfo='label+text+value', selector=dict(type='treemap'),textfont=dict(size=15))
+    # Add " documents" text after the document count
+    fig3.update_traces(texttemplate='<b>%{label}</b>'+ '<br><b>%{value}</b> documents', selector=dict(type='treemap'))
+    fig3.update_traces(marker=dict(cornerradius=5))
+    st.plotly_chart(fig3, use_container_width=True)     
 
 
 # first row
@@ -120,7 +110,6 @@ with c1:
     # )
 
 with c2:
-    st.title('Word Cloud Generator from Dataset')
     def generate_wordcloud(data):
         text = ' '.join(data)
         wordcloud = WordCloud(width=800, height=400, background_color='white').generate(text)
@@ -168,104 +157,70 @@ with c3:
     # )
 
 ##################################################
-# Row A
-st.markdown('### Metrics')
-col1, col2, col3 = st.columns(3)
-col1.metric("Temperature", "70 °F", "1.2 °F")
-col2.metric("Wind", "9 mph", "-8%")
-col3.metric("Humidity", "86%", "4%")
+# # Row A
+# st.markdown('### Metrics')
+# col1, col2, col3 = st.columns(3)
+# col1.metric("Temperature", "70 °F", "1.2 °F")
+# col2.metric("Wind", "9 mph", "-8%")
+# col3.metric("Humidity", "86%", "4%")
 
 
 
-#############################################################################################################################
-    
-# Sample data for filtered_df
-    
+# ##first row 
+# c1, c2 = st.columns((5,5))
+# with c1:
+#    # Create a horizontal bar chart with thin bars and reduced spacing
+#     fig, ax = plt.subplots(figsize=(8, 4))  # Set figure size to make it smaller
+#     pastel_colors = plt.cm.get_cmap('Pastel1', len(top_10_data))
+#     bars = ax.barh(top_10_data['Country'], top_10_data['Document Count'], color=pastel_colors.colors, height=0.4)  # Adjust bar height for thin bars
 
-np.random.seed(42)  # Set seed for reproducibility
+#     # Display the values on top of each bar
+#     for i, val in enumerate(top_10_data['Document Count']):
+#         ax.text(val, i, str(val), ha='left', va='center', fontsize=10)  # Display values on bars
 
-regions = np.random.choice(['North', 'South', 'East', 'West'], size=100)
-categories = np.random.choice(['Electronics', 'Clothing', 'Furniture'], size=100)
-sub_categories = np.random.choice(['Phones', 'Laptops', 'Shirts', 'Chairs'], size=100)
-sales = np.random.randint(100, 1000, size=100)
+#     # Customize appearance: Remove background gridlines and spines
+#     ax.grid(False)
+#     ax.spines['top'].set_visible(False)
+#     ax.spines['right'].set_visible(False)
+#     ax.spines['left'].set_visible(False)  # Hide the y-axis line
 
-filtered_df = pd.DataFrame({
-    'Region': regions,
-    'Category': categories,
-    'Sub-Category': sub_categories,
-    'Sales': sales
-})
+#     # Hide x-axis line and labels
+#     ax.xaxis.set_visible(False)
 
+#     # Set y-axis label and title
+#     ax.set_title('Horizontal Bar Chart', fontsize=14)
 
-st.subheader("Hierarchical view of Sales using TreeMap")
-fig3 = px.treemap(filtered_df, path = ["Region","Category","Sub-Category"], values = "Sales",hover_data = ["Sales"],
-                  color = "Sub-Category")
-fig3.update_layout(width = 800, height = 650)
-st.plotly_chart(fig3, use_container_width=True)
-#############################################################################################################################
-
-
-##first row 
-c1, c2 = st.columns((5,5))
-with c1:
-   # Sample data for horizontal bar chart
-    bar_data = pd.DataFrame({
-        'Category': ['Andorra', 'Belgica', 'Canada', 'Dinamarca', 'Equador', 'França', 'Portugal'],
-        'Values': [23, 45, 56, 78, 12, 34, 65]
-    })
-
-    # Create a horizontal bar chart with thin bars and reduced spacing
-    fig, ax = plt.subplots(figsize=(8, 4))  # Set figure size to make it smaller
-    pastel_colors = plt.cm.get_cmap('Pastel1', len(bar_data))
-    bars = ax.barh(bar_data['Category'], bar_data['Values'], color=pastel_colors.colors, height=0.2)  # Adjust bar height for thin bars
-
-    # Display the values on top of each bar
-    for i, val in enumerate(bar_data['Values']):
-        ax.text(val, i, str(val), ha='left', va='center', fontsize=10)  # Display values on bars
-
-    # Customize appearance: Remove background gridlines and spines
-    ax.grid(False)
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
-    ax.spines['left'].set_visible(False)  # Hide the y-axis line
-
-    # Hide x-axis line and labels
-    ax.xaxis.set_visible(False)
-
-    # Set y-axis label and title
-    ax.set_title('Horizontal Bar Chart', fontsize=14)
-
-    # Display the bar chart using Streamlit
-    st.pyplot(fig)
+#     # Display the bar chart using Streamlit
+#     st.pyplot(fig)
 
  
-with c2:
-     # Sample data for horizontal bar chart
-    bar_data = pd.DataFrame({
-        'Category': ['Andorra', 'Belgica', 'Canada', 'Dinamarca', 'Equador', 'França', 'Portugal'],
-        'Values': [23, 45, 56, 78, 12, 34, 65]
-    })
+# with c2:
+#      # Sample data for horizontal bar chart
+#     bar_data = pd.DataFrame({
+#         'Category': ['Andorra', 'Belgica', 'Canada', 'Dinamarca', 'Equador', 'França', 'Portugal'],
+#         'Values': [23, 45, 56, 78, 12, 34, 65]
+#     })
 
-    # Create a horizontal bar chart with thin bars and reduced spacing
-    fig, ax = plt.subplots(figsize=(8, 4))  # Set figure size to make it smaller
-    pastel_colors = plt.cm.get_cmap('Pastel1', len(bar_data))
-    bars = ax.barh(bar_data['Category'], bar_data['Values'], color=pastel_colors.colors, height=0.2)  # Adjust bar height for thin bars
+#     # Create a horizontal bar chart with thin bars and reduced spacing
+#     fig, ax = plt.subplots(figsize=(8, 4))  # Set figure size to make it smaller
+#     pastel_colors = plt.cm.get_cmap('Pastel1', len(bar_data))
+#     bars = ax.barh(bar_data['Category'], bar_data['Values'], color=pastel_colors.colors, height=0.2)  # Adjust bar height for thin bars
 
-    # Display the values on top of each bar
-    for i, val in enumerate(bar_data['Values']):
-        ax.text(val, i, str(val), ha='left', va='center', fontsize=10)  # Display values on bars
+#     # Display the values on top of each bar
+#     for i, val in enumerate(bar_data['Values']):
+#         ax.text(val, i, str(val), ha='left', va='center', fontsize=10)  # Display values on bars
 
-    # Customize appearance: Remove background gridlines and spines
-    ax.grid(False)
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
-    ax.spines['left'].set_visible(False)  # Hide the y-axis line
+#     # Customize appearance: Remove background gridlines and spines
+#     ax.grid(False)
+#     ax.spines['top'].set_visible(False)
+#     ax.spines['right'].set_visible(False)
+#     ax.spines['left'].set_visible(False)  # Hide the y-axis line
 
-    # Hide x-axis line and labels
-    ax.xaxis.set_visible(False)
+#     # Hide x-axis line and labels
+#     ax.xaxis.set_visible(False)
 
-    # Set y-axis label and title
-    ax.set_title('Horizontal Bar Chart', fontsize=14)
+#     # Set y-axis label and title
+#     ax.set_title('Horizontal Bar Chart', fontsize=14)
 
-    # Display the bar chart using Streamlit
-    st.pyplot(fig)
+#     # Display the bar chart using Streamlit
+#     st.pyplot(fig)
